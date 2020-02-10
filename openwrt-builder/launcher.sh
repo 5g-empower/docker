39ee8225dfc75d222c78e3b6e63cf77df6ebed4a
cnet <g.baggio@create-net.org>
2020-02-10 09:33:36 +0100

    add openwrt-builder

---
+++ openwrt-builder/launcher.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+env
+
+git clone https://github.com/5g-empower/empower-openwrt.git ~/openwrt
+git clone https://github.com/5g-empower/empower-configs.git ~/empower-configs
+
+cd ~/openwrt
+./scripts/feeds update -a
+./scripts/feeds install -a
+mkdir files
+
+TARGETS="apu2d wdr4300 wndr4300 alix2d"
+
+for TARGET in $TARGETS; do
+
+	echo "Configuring target $TARGET"
+
+	cp ~/empower-configs/$TARGET.config ~/openwrt/.config
+	cp -r ~/empower-configs/empower-files-$TARGET/* ~/openwrt/files
+
+	make defconfig
+
+	echo "Begin compilation"
+	make -j1 V=s
+	echo "End compilation"
+
+done
+
+FOLDER_NAME="$(date +"%Y-%m-%d-%H:%M")"
+rm -r /builds/$FOLDER_NAME
+mkdir /builds/$FOLDER_NAME
+cp -r ~/openwrt/bin /builds/$FOLDER_NAME/
+
